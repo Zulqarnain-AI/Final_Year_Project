2b789d550fa1eb912a8cf0299f62f56cc119d335
Zulqarnain-AIgit config --global user.name Zulqarnain-AIgit config --global user.name Zulqarnain-AI <qarnainhassan786@gmail.com>
2025-12-14 12:00:07 +0500

    environental alert module added

--- Frontend/src/components/environmental_alert/api_data_fetching.jsx
+++ Frontend/src/components/environmental_alert/api_data_fetching.jsx
@@ -1,8 +1,5 @@
-
-export default async function fetch_aqi_data() {
-    const api_url = "http://api.weatherapi.com/v1/current.json?key=b22130263a3a449a8d0155902251312&q=Islamabad&aqi=yes";
-
-    let response = await fetch(api_url)
-
-    return await response.json();
+export default async function fetch_aqi_data(city) {
+  const api_url = `http://api.weatherapi.com/v1/current.json?key=b22130263a3a449a8d0155902251312&q=${city}&aqi=yes`;
+  const response = await fetch(api_url);
+  return await response.json();
 }
